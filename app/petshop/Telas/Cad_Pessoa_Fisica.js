6101cb52f3415062564ad3b6e19495eaf3e86324
jooj_txt <72229153+jooj-txt@users.noreply.github.com>
2023-08-15 14:27:47 +0000

    terminando

--- app/petshop/Telas/Cad_Pessoa_Fisica.js
+++ app/petshop/Telas/Cad_Pessoa_Fisica.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, CheckBox, StyleSheet } from 'react-native';
 
 const PessoaFisicaCadastroScreen = () => {
   const [nome, setNome] = useState('');
   const [cpf, setCpf] = useState('');
   const [email, setEmail] = useState('');
+  const [celular, setCelular] = useState('');
+  const [sexo, setSexo] = useState('');
+  const [dataNascimento, setDataNascimento] = useState('');
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [estado, setEstado] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [aceitarTermos, setAceitarTermos] = useState(false);
 
   const handleCadastro = () => {
     // Lógica de cadastro aqui
-    console.log('Dados de cadastro:', nome, cpf, email);
+    console.log('Dados de cadastro:', nome, cpf, email, celular, sexo, dataNascimento, cep, endereco, cidade, estado);
   };
 
   return (
@@ -32,9 +42,72 @@ const PessoaFisicaCadastroScreen = () => {
         value={email}
         onChangeText={setEmail}
       />
-      <Pressable style={styles.button} onPress={handleCadastro}>
+      <TextInput
+        style={styles.input}
+        placeholder="Celular"
+        value={celular}
+        onChangeText={setCelular}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Sexo"
+        value={sexo}
+        onChangeText={setSexo}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Data de Nascimento"
+        value={dataNascimento}
+        onChangeText={setDataNascimento}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="CEP"
+        value={cep}
+        onChangeText={setCep}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Endereço"
+        value={endereco}
+        onChangeText={setEndereco}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Cidade"
+        value={cidade}
+        onChangeText={setCidade}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Estado"
+        value={estado}
+        onChangeText={setEstado}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        onChangeText={setSenha}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirmar Senha"
+        secureTextEntry
+        value={confirmarSenha}
+        onChangeText={setConfirmarSenha}
+      />
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          value={aceitarTermos}
+          onValueChange={setAceitarTermos}
+        />
+        <Text style={styles.checkboxLabel}>Aceitar termos e condições</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleCadastro}>
         <Text style={styles.buttonText}>Cadastrar</Text>
-      </Pressable>
+      </TouchableOpacity>
     </View>
   );
 };
