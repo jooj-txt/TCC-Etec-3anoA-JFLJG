89bb40028c10e35516d3960a56f4aef7b42c5f5d
fefsares <nandicoares@gmail.com>
2023-11-19 12:04:15 -0300

    super glow up no site

--- site2/script.js
+++ site2/script.js
@@ -1,1 +1,11 @@
 AOS.init();
+
+ var nav = document.querySelector('nav1');
+
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('tccNavbar', 'shadow');
+        } else {
+          nav.classList.remove('tccNavbar', 'shadow');
+        }
+      });
